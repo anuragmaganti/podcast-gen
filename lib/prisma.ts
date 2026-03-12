be998c474b94d872154865e8bd7b484296c94f4d
anuragmaganti <amaganti.dev@gmail.com>
2026-03-12 18:37:57 -0400

    backup before reset

---
+++ lib/prisma.ts
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@/lib/generated/prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
+
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}

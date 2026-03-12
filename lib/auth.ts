4dc333beda0307e358fe7c07419ba0c65e352978
anuragmaganti <amaganti.dev@gmail.com>
2026-03-12 18:52:47 -0400

    delete prev files

--- lib/auth.ts
+++
@@ -1,9 +0,0 @@
-import { betterAuth } from "better-auth";
-import { prismaAdapter } from "@better-auth/prisma-adapter";
-import { prisma } from "@/lib/prisma";
-
-export const auth = betterAuth({
-  database: prismaAdapter(prisma, {
-    provider: "postgresql", // or "mysql", "postgresql", ...etc
-  }),
-});

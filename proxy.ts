e3984df49bc2a9a9eb117262fa58f4ec80b97790
anuragmaganti <amaganti.dev@gmail.com>
2026-03-13 00:08:44 -0400

    add shadcn auth components: sign-in, sign-up, user button. Add protection at homepage - to be changed to public

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,6 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware(async (auth) => {
-  await auth.protect();
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
